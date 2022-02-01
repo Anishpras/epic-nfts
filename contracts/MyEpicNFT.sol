987d2d3d36f85fb0f3823a7d4cf2cd87a99bb732
Anish Prashun <66175058+Anishpras@users.noreply.github.com>
2022-02-01 11:01:46 +0530

    creating contract

--- contracts/MyEpicNFT.sol
+++ contracts/MyEpicNFT.sol
@@ -2,10 +2,29 @@
 
 pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 import "hardhat/console.sol";
 
-contract MyEpicNft {
-    constructor(){
-        console.log("This is my NFT contract. Whoa!");
-    }
+contract MyEpicNFT is ERC721URIStorage {
+   
+   using Counters for Counters.Counter;
+   Counters.Counter private _tokenIds;
+
+    constructor() ERC721 ("SquareNFT", "SQUARE") {
+    console.log("This is my NFT contract. Woah!");
+  }
+  function makeAnEpicNFT() public {
+     // Get the current tokenId, this starts at 0.
+    uint256 newItemId = _tokenIds.current();
+
+     // Actually mint the NFT to the sender using msg.sender.
+    _safeMint(msg.sender, newItemId);
+
+    // Set the NFTs data.
+    _setTokenURI(newItemId, "blah");
+
+    // Increment the counter for when the next NFT is minted.
+    _tokenIds.increment();
+  }
 }
