67c9cc7a7af3ee13a97c86d096adf2b52bd80fdc
Anish Prashun <66175058+Anishpras@users.noreply.github.com>
2022-02-20 16:08:00 +0530

    nft contract changed

--- contracts/MyEpicNFT.sol
+++ contracts/MyEpicNFT.sol
@@ -22,9 +22,10 @@ contract MyEpicNFT is ERC721URIStorage {
     _safeMint(msg.sender, newItemId);
 
     // Set the NFTs data.
-    _setTokenURI(newItemId, "blah");
+   _setTokenURI(newItemId, "https://jsonkeeper.com/b/RUUS");
 
     // Increment the counter for when the next NFT is minted.
+    console.log("An NFT w/ ID %s has been minted to %s", newItemId, msg.sender);
     _tokenIds.increment();
   }
 }
